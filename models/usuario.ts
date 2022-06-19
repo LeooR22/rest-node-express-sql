905081d6ce86c992385d26df3b9562271b12d012
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-06-19 20:51:59 -0300

    usuario models updated

--- models/usuario.ts
+++ models/usuario.ts
@@ -4,9 +4,12 @@ import db from "../db/connection";
 const Usuario = db.define("Usuario", {
   nombre: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   email: {
     type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
   },
   estado: {
     type: DataTypes.BOOLEAN,
